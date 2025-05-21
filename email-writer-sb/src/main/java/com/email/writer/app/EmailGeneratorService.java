98e03112688800c03ac7d587dc91b9049515c3ba
AryanS-88 <ssingharyan70@gmail.com>
2025-05-21 20:52:28 +0530

    Contoller and services setup

---
+++ email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorService.java
@@ -0,0 +1,12 @@
+package com.email.writer.app;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailGeneratorService {
+    @Override
+    public String generateEmailReply(EmailRequest emailRequest) {
+        // build the Prompt
+
+    }
+}
