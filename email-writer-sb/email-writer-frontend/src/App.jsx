9924589f16322cbf42fdd8f4e3d1fc7aa151be4c
AryanS-88 <ssingharyan70@gmail.com>
2025-05-23 10:02:13 +0530

    copy to clipboard functionality

--- email-writer-sb/email-writer-frontend/src/App.jsx
+++ email-writer-sb/email-writer-frontend/src/App.jsx
@@ -1,89 +1,122 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import {
-  Typography,
-  Container,
-  TextField,
   Box,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-  CircularProgress,
   Button,
+  CircularProgress,
+  Container,
+  FormControl,
+  Input,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
 } from '@mui/material'
+import axios from 'axios'
 
-const App = () => {
+function App() {
   const [emailContent, setEmailContent] = useState('')
   const [tone, setTone] = useState('')
-  const [generateReply, setGeneratedReply] = useState('')
+  const [generatedReply, setGeneratedReply] = useState('')
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState('')
 
-  const handleSubmit = async () => {}
+  const handleSubmit = async () => {
+    setLoading(true)
+    setError('')
+    try {
+      const response = await axios.post(
+        'http://localhost:8080/api/email/generate',
+        {
+          emailContent,
+          tone,
+        }
+      )
+      setGeneratedReply(
+        typeof response.data === 'string'
+          ? response.data
+          : JSON.stringify(response.data)
+      )
+    } catch (error) {
+      setError('Failed to generate eamil reply. Please try again')
+      console.error(error)
+    } finally {
+      setLoading(false)
+    }
+  }
 
   return (
-    <div>
-      <Container maxWidth="md" sx={{ py: 4 }}>
-        <Typography variant="h3" component="h1" gutterBottom>
-          Email Reply Generator
+    <Container maxWidth="md" sx={{ py: 4 }}>
+      <Typography variant="h3" component="h1" gutterBottom>
+        Email Reply Generator
+      </Typography>
+
+      <Box sx={{ mx: 3 }}>
+        <TextField
+          fullWidth
+          multiline
+          rows={6}
+          variant="outlined"
+          label="Original Email Content"
+          value={emailContent || ''}
+          onChange={(e) => setEmailContent(e.target.value)}
+          sx={{ mb: 2 }}
+        />
+
+        <FormControl fullWidth sx={{ mb: 2 }}>
+          <InputLabel>Tone (Optional)</InputLabel>
+          <Select
+            value={tone || ''}
+            label={'Tone (Optional)'}
+            onChange={(e) => setTone(e.target.value)}
+          >
+            <MenuItem value="">None</MenuItem>
+            <MenuItem value="professional">Professional</MenuItem>
+            <MenuItem value="casual">Casual</MenuItem>
+            <MenuItem value="friendly">Friendly</MenuItem>
+          </Select>
+        </FormControl>
+
+        <Button
+          variant="contained"
+          onClick={handleSubmit}
+          disabled={!emailContent || loading}
+          fullWidth
+        >
+          {loading ? <CircularProgress size={24} /> : 'Generate Reply'}
+        </Button>
+      </Box>
+
+      {error && (
+        <Typography color="error" sx={{ mb: 2 }}>
+          {error}
         </Typography>
-        <Box sx={{ mx: 3 }}>
+      )}
+
+      {generatedReply && (
+        <Box sx={{ mt: 3 }}>
+          <Typography variant="h6" gutterBottom>
+            Generated Reply:
+          </Typography>
           <TextField
             fullWidth
             multiline
             rows={6}
             variant="outlined"
-            label="Original Email Content"
-            value={emailContent}
-            onChange={(e) => setEmailContent(e.target.value)}
-            sx={{ mb: 2 }}
+            value={generatedReply || ''}
+            inputProps={{ readOnly: true }}
           />
-          <FormControl fullWidth sx={{ mb: 2 }}>
-            <InputLabel>Tone (Optional)</InputLabel>
-            <Select
-              value={tone}
-              label="Tone (Optional)"
-              onChange={(e) => setTone(e.target.value)}
-            >
-              <MenuItem value="professional">Professional</MenuItem>
-              <MenuItem value="casual">Casual</MenuItem>
-              <MenuItem value="friendly">Friendly</MenuItem>
-              <MenuItem value="concise">Concise</MenuItem>
-            </Select>
-          </FormControl>
+
           <Button
-            variant="contained"
-            onClick={handleSubmit}
-            disabled={!emailContent || loading}
+            variant="outlined"
+            sx={{ mt: 2 }}
+            onClick={() => navigator.clipboard.writeText(generatedReply)}
           >
-            {loading ? <CircularProgress size={24} /> : 'Generated Reply'}
+            Copy to Clipboard
           </Button>
         </Box>
-
-        {error && (
-          <Typography color="error" sx={{ mb: 2 }}>
-            {error}
-          </Typography>
-        )}
-
-        {generateReply && (
-          <Box sx={{ mt: 3 }}>
-            <Typography variant="h6" gutterBottom>
-              Genreated reply:
-            </Typography>
-            <TextField
-              fullWidth
-              multiline
-              rows={6}
-              variant="outlined"
-              value={generateReply || ''}
-              inputProps={{ readOnly: true }}
-            />
-            <Button>Copy to Clipboard</Button>
-          </Box>
-        )}
-      </Container>
-    </div>
+      )}
+    </Container>
   )
 }
 
